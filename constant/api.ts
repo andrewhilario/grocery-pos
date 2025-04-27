0a8ad55df28bfa2d4d29b16f65ac3e07a1c99a47
Drew <hilarioandrew12@gmail.com>
2025-04-27 18:16:37 +0800

    fix: update API_URL to use the correct production endpoint

--- constant/api.ts
+++ constant/api.ts
@@ -1,2 +1,2 @@
-export const API_URL = "http://localhost:8000/api/v1";
-// export const API_URL = "https://grocery-pos-backend-five.vercel.app/api/v1";
+// export const API_URL = "http://localhost:8000/api/v1";
+export const API_URL = "https://grocery-pos-backend-five.vercel.app/api/v1";
